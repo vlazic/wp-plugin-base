4c6c2b78e6d501b7aa53f33c6d232d1511386e29
Vladimir Lazić <vlazic.work@gmail.com>
2017-11-11 04:49:03 +0100

    fixed Templates class

--- include/Templates.php
+++ include/Templates.php
@@ -9,10 +9,10 @@ class Templates
         // function starts with 'return' and we will return compiled template
         if (0 === strpos($template, 'return')) {
             ob_start();
-            include_once FT_TEMPLATES . $template . '.php';
+            include_once REPLACE_PLUGIN_NAMESPACE_TEMPLATES . substr($template, 6) . '.php';
             return ob_get_clean();
         }
         // echo out compiled template
-        include_once FT_TEMPLATES . $template . '.php';
+        include_once REPLACE_PLUGIN_NAMESPACE_TEMPLATES . $template . '.php';
     }
 }
